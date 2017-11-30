9c29af0f218d02eedb2a4b8fd671345d8fbf6acc
Claudio Spizzi <claudio@spizzi.net>
2017-11-30 23:35:23 +0100

    Suppress script analyer rules

--- Modules/SharePointFever/Tests/Unit/New-SPListItem.Tests.ps1
+++ Modules/SharePointFever/Tests/Unit/New-SPListItem.Tests.ps1
@@ -1,4 +1,7 @@
 ﻿
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
+param ()
+
 $modulePath = Resolve-Path -Path "$PSScriptRoot\..\..\.." | Select-Object -ExpandProperty Path
 $moduleName = Resolve-Path -Path "$PSScriptRoot\..\.." | Get-Item | Select-Object -ExpandProperty BaseName
 
